88220890dee9858d0feeb5544edb536f976b3ed4
Thales Euler <thaleslevs@gmail.com>
2024-05-10 01:30:09 -0300

    segunda solução

--- exceptions-reservations/src/application/Main.java
+++ exceptions-reservations/src/application/Main.java
@@ -42,18 +42,14 @@ public class Main  {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.nextLine());
 
-            Date now = new Date();
+            String error = reservation.updateDates(checkIn, checkOut);
 
-            if (checkIn.before(now) || checkOut.before(now))
-                System.out.println("Error in reservation: Reservation dates for update must be future.");
+            if (error != null)
+                System.out.println("Error in reservation: " + error);
 
-            else if (!checkOut.after(checkIn))
-                System.out.println("Error in reservation: Check-out date must be after check-in date.");
+            else
+                System.out.println("Reservartion: " + reservation);
 
-            else {
-                reservation.updateDates(checkIn, checkOut);
-                System.out.println(reservation);
-            }
         }
 
 
